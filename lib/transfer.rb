5a8110d62b68eba6462aea7e221563087dbc9fe4
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-02 23:14:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
 
-  attr_accessor :name, :new_name
+  attr_accessor :name, :new_name, :amount
 
   def initialize
     @name = name
-
+    @new_name = new_name
+    @amount = amount
   end
 
 
